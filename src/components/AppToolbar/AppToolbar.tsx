801d99a0eacc15d554eff4a49663d7b4621446af
ShenyiCui <shenyicui@outlook.com>
2022-08-23 23:44:03 +0800

    feat: add menubar hamburger to toolbar
    - add menubar to toolbar
    
    addresses [Create Add-Ons Page - Mobile]

--- src/components/AppToolbar/AppToolbar.tsx
+++ src/components/AppToolbar/AppToolbar.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { IonHeader, IonToolbar } from '@ionic/react';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton } from '@ionic/react';
 import ScootLogo from '@components/ScootLogo';
 
 const AppToolbar = () => {
   return (
     <IonHeader>
       <IonToolbar color='primary' mode='ios' className='h-16 flex'>
-        <ScootLogo className='ml-4' />
+        <ScootLogo className='ml-5' />
+        <IonButtons slot='end'>
+          <IonMenuButton className='mr-3' auto-hide='false' />
+        </IonButtons>
       </IonToolbar>
     </IonHeader>
   );
