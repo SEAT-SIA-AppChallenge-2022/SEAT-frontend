1748b19d952bde75c65d16f5285110215fcc13a1
ShenyiCui <shenyicui@outlook.com>
2022-08-24 22:23:08 +0800

    feat: add navigation states to constants
    - add navigation states to constants
    
    addresses [Create Add-Ons Page - Mobile]

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,32 @@
+import cartIconFilled from '@/assets/icons/cart-icon-filled.svg';
+import dollarIconFilled from '@/assets/icons/dollar-icon-filled.svg';
+import shopIconFilled from '@/assets/icons/shop-icon-filled.svg';
+import dollarIconUnFilled from '@/assets/icons/dollar-icon-unfilled.svg';
+import shopIconUnFilled from '@/assets/icons/shop-icon-unfilled.svg';
+import { NavigationState } from '@components/TopNavigationBar/TopNavigationBar';
+
 export const severity = {
   SUCCESS: 'success',
   LOADING: 'loading',
   ERROR: 'error',
   WARNING: 'warning',
   INFO: 'info',
+};
+
+export const navigationStates: NavigationState = {
+  pickAttractions: {
+    icon1: cartIconFilled,
+    icon2: dollarIconUnFilled,
+    icon3: shopIconUnFilled,
+  },
+  reviewCart: {
+    icon1: cartIconFilled,
+    icon2: dollarIconFilled,
+    icon3: shopIconUnFilled,
+  },
+  completePayment: {
+    icon1: cartIconFilled,
+    icon2: dollarIconFilled,
+    icon3: shopIconFilled,
+  },
 };
