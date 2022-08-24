48dd99d56867b121940680e36045b2d92c82ea85
ShenyiCui <shenyicui@outlook.com>
2022-08-24 22:18:59 +0800

    feat: add top navigation bar logic
    - add top navigation bar
    
    addresses [Create Add-Ons Page - Mobile]

--- src/components/TopNavigationBar/TopNavigationBar.tsx
+++ src/components/TopNavigationBar/TopNavigationBar.tsx
@@ -3,27 +3,40 @@ import React from 'react';
 import { IonIcon } from '@ionic/react';
 
 import rightCaret from '@/assets/icons/right-arrow-icon.svg';
-import dollarIcon from '@/assets/icons/dollar-icon.svg';
-import cartIcon from '@/assets/icons/cart-icon.svg';
-import shopIcon from '@/assets/icons/shop-icon.svg';
 
-const TopNavigationBar = () => {
+type IconState = {
+  icon1: string;
+  icon2: string;
+  icon3: string;
+};
+
+export type NavigationState = {
+  pickAttractions: IconState;
+  reviewCart: IconState;
+  completePayment: IconState;
+};
+
+type Props = {
+  state: IconState;
+};
+
+const TopNavigationBar = ({ state }: Props) => {
   return (
     <div className='grid grid-cols-5 sticky top-0 h-16 bg-white z-30 drop-shadow-md'>
       <div className='flex justify-center items-center'>
-        <IonIcon className='w-6 h-6' icon={cartIcon} />
+        <IonIcon className='w-6 h-6 cursor-pointer' icon={state.icon1} />
       </div>
       <div className='flex justify-center items-center'>
         <IonIcon className='w-6 h-6' icon={rightCaret} />
       </div>
       <div className='flex justify-center items-center'>
-        <IonIcon className='w-6 h-6' icon={shopIcon} />
+        <IonIcon className='w-6 h-6 cursor-pointer' icon={state.icon2} />
       </div>
       <div className='flex justify-center items-center'>
         <IonIcon className='w-6 h-6' icon={rightCaret} />
       </div>
       <div className='flex justify-center items-center'>
-        <IonIcon className='w-6 h-6' icon={dollarIcon} />
+        <IonIcon className='w-6 h-6 cursor-pointer' icon={state.icon3} />
       </div>
     </div>
   );
