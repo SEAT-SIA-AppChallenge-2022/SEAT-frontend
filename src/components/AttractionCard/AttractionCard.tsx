d738192a00d7f847fe712e678ee2842aebba57d6
ShenyiCui <shenyicui@outlook.com>
2022-08-24 21:22:14 +0800

    feat: add attraction card component
    - add attraction card component
    
    addresses [Create Add-Ons Page - Mobile]

---
+++ src/components/AttractionCard/AttractionCard.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { IonCard, IonCardSubtitle, IonButton } from '@ionic/react';
+
+type props = {
+  className?: string;
+  title: string;
+  imgUrl: string;
+  price?: string;
+};
+
+const AttractionCard = ({ className, title, imgUrl, price }: props) => {
+  return (
+    <IonCard className={`font-helvetica relative ${className}`}>
+      <img src={imgUrl} />
+      <div className='absolute bg-transparentGrey bottom-0 py-3 px-8 w-full'>
+        <IonCardSubtitle mode='md' className='text-white font-helvetica text-sm font-semibold'>
+          {title}
+        </IonCardSubtitle>
+        <div className='grid grid-cols-2 gap-4'>
+          <div className='flex items-center'>
+            <p className='text-white font-helvetica font-semibold'>From SGD {price}</p>
+          </div>
+          <div>
+            <IonButton mode='ios' className='text-xs font-semibold w-full'>
+              View
+            </IonButton>
+          </div>
+        </div>
+      </div>
+    </IonCard>
+  );
+};
+
+export default AttractionCard;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import { IonCard, IonCardSubtitle, IonButton } from '@ionic/react';
-
 import PageWithHeader from '@components/PageWithHeader';
 import BackgroundBlur from '@components/BackgroundBlur';
 import BackgroundImg from '@components/BackgroundImg';
-import OptionsDropdown from '@/components/OptionsDropdown';
+import OptionsDropdown from '@components/OptionsDropdown';
 import TopNavigationBar from '@components/TopNavigationBar';
+import AttractionCard from '@components/AttractionCard';
 
 import lobster from '@/assets/imgs/lobster.jpeg';
 
@@ -26,25 +25,7 @@ const Home: React.FC = () => {
 
             <OptionsDropdown className='mt-8' />
           </div>
-
-          <IonCard className='font-helvetica relative'>
-            <img src={lobster} />
-            <div className='absolute bg-transparentGrey bottom-0 py-3 px-8 w-full'>
-              <IonCardSubtitle mode='md' className='text-white font-helvetica text-sm font-semibold'>
-                Dining overlooking Syndney CBD: Infinity at Sydney Tower
-              </IonCardSubtitle>
-              <div className='grid grid-cols-2 gap-4'>
-                <div className='flex items-center'>
-                  <p className='text-white font-helvetica font-semibold'>From SGD 13.50</p>
-                </div>
-                <div>
-                  <IonButton mode='ios' className='text-xs font-semibold w-full'>
-                    View
-                  </IonButton>
-                </div>
-              </div>
-            </div>
-          </IonCard>
+          <AttractionCard className='mt-8' title='Something cool!' imgUrl={lobster} price='13.50' />
         </div>
       </div>
     </PageWithHeader>
