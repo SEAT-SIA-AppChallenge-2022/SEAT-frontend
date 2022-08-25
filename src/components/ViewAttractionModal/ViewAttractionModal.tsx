5b710f1a8b79eac3f16b1879554dafe4b6be99af
ShenyiCui <shenyicui@outlook.com>
2022-08-25 20:57:16 +0800

    feat: add clickaway listener logic to modal
    - add clickaway listener logic to modal
    - add react-clickaway library

--- src/components/ViewAttractionModal/ViewAttractionModal.tsx
+++ src/components/ViewAttractionModal/ViewAttractionModal.tsx
@@ -2,40 +2,61 @@ import React from 'react';
 
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
 import { useSelector, useDispatch } from 'react-redux';
+import ClickAwayListener from 'react-click-away-listener';
 
 import Button from '@components/Button';
 import DateTimePicker from '@components/DateTimePicker';
 import { hideAttractionModal, getIsAttractionModalOpen } from '@/store/ui/uiSlice';
+import { AttractionCategory } from '@/types/attractions/attractions';
 
-import lobster from '@/assets/imgs/lobster.jpeg';
+type Props = {
+  category: AttractionCategory;
+  title: string;
+  description: string;
+  imgUrl: string;
+  id: number;
+};
 
-const ViewAttractionModal = () => {
+const ViewAttractionModal = ({ category, title, description, id, imgUrl }: Props) => {
   const dispatch = useDispatch();
   const isOpen = useSelector(getIsAttractionModalOpen);
+
+  const handleAddClick = () => {
+    console.log(id);
+    dispatch(hideAttractionModal());
+  };
+
+  const handleClickAway = () => {
+    dispatch(hideAttractionModal());
+  };
+
   return (
-    <div onClick={() => dispatch(hideAttractionModal())} className={isOpen ? `absolute z-50 w-screen h-screen bg-[#000000b3]` : `hidden`}>
-      <IonCard className='mt-12'>
-        <img src={lobster} />
-        <IonCardHeader>
-          <IonCardSubtitle mode='ios'>Activity</IonCardSubtitle>
-          <IonCardTitle mode='ios'>Madison, WI</IonCardTitle>
-        </IonCardHeader>
-        <IonCardContent mode='ios'>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-          enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-          in culpa qui officia deserunt mollit anim id est laborum.
-          <div className='flex w-full justify-end mt-5'>
-            <DateTimePicker />
-          </div>
-          <div className='flex w-full justify-end'>
-            <Button onClick={() => dispatch(hideAttractionModal())} className='w-32'>
-              Add
-            </Button>
-          </div>
-        </IonCardContent>
-      </IonCard>
-    </div>
+    isOpen && (
+      <>
+        <div className={`absolute z-40 w-screen h-screen bg-[#000000b3]`}>
+          <ClickAwayListener onClickAway={handleClickAway}>
+            <IonCard className='mt-12 z-50'>
+              <img src={imgUrl} />
+              <IonCardHeader>
+                <IonCardSubtitle mode='ios'>{category}</IonCardSubtitle>
+                <IonCardTitle mode='ios'>{title}</IonCardTitle>
+              </IonCardHeader>
+              <IonCardContent mode='ios'>
+                {description}
+                <div className='flex w-full justify-end mt-5'>
+                  <DateTimePicker />
+                </div>
+                <div className='flex w-full justify-end'>
+                  <Button onClick={handleAddClick} className='w-32'>
+                    Add
+                  </Button>
+                </div>
+              </IonCardContent>
+            </IonCard>
+          </ClickAwayListener>
+        </div>
+      </>
+    )
   );
 };
 
