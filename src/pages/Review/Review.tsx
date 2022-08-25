062a21545eb3609d6010104fc2d726fa2a271f54
ShenyiCui <shenyicui@outlook.com>
2022-08-26 06:26:06 +0800

    fix: fix logic for restarting demo
    - fix restart demo logic

--- src/pages/Review/Review.tsx
+++ src/pages/Review/Review.tsx
@@ -16,11 +16,9 @@ import { ChosenAttraction } from '@/types/attractions/attractions';
 import DateTime from '@/types/DateTime/DateTime';
 
 import { IonFooter } from '@ionic/react';
-import { useHistory } from 'react-router';
 import { useSelector } from 'react-redux';
 
 const Review = () => {
-  const history = useHistory();
   const chosenAttractions = useSelector(getChosenAttractions) ?? [];
   const chosenAttractionsCopy = [...chosenAttractions];
   chosenAttractionsCopy.sort((x, y) => new Date(x.chosenDate.toString()).getTime() - new Date(y.chosenDate.toString()).getTime());
@@ -78,7 +76,7 @@ const Review = () => {
       </div>
 
       <IonFooter className='px-4 flex items-center z-40 fixed bottom-0 h-24 bg-transparentGrey'>
-        <Button disabled={chosenAttractions.length === 0} onClick={() => history.push(Routes.addOns)} className='w-full'>
+        <Button disabled={chosenAttractions.length === 0} onClick={() => location.reload()} className='w-full'>
           Restart Demo
         </Button>
       </IonFooter>
